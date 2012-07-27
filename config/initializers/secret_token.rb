2f4d7012d5f80558a5a6fa981cc01acd1fad5c46
TJ Peden <tj.peden@tj-coding.com>
2012-07-27 12:53:14 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BnbTest::Application.config.secret_token = 'ae813326cbeaca89b4ff61054e226878a38075978234070f569229c3c63d6f3d21008a205fa86958baff3a840f1a1ccefd8e4774ab1c1c3c165ea8c06aac439d'
