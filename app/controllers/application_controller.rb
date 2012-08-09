1835dea530d4b2aca6ce6b72eb8663c7716afc65
TJ Peden <tj.peden@tj-coding.com>
2012-08-09 12:19:36 -0500

    A giant step for Rails kind

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+private
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def authenticate_user!
+    if !current_user
+      redirect_to root_path, error: "Please log in to use this app"
+    end
+  end
+  
+  helper_method :current_user
 end
